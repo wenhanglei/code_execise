2bf81e9f355e2aefb507aa07d0ea22d900be7914
wenhanglei <3188744608@qq.com>
2017-08-16 17:14:52 +0800

    load my activation code into mysql database

--- 002/add_data_into_db.py
+++ 002/add_data_into_db.py
@@ -1,15 +1,25 @@
 from mysql import connector
+from generate_activation_code import activation_code
 
+#获得mysql数据库连接
 cnx = connector.connect(user='root',
-                        password='712342',
+                        password='****',
                         host='localhost',
-                        database='mysql')
-cnx.database = 'mytest'
+                        database='test')
+#创建光标对象
 cursor = cnx.cursor()
-add_account = "insert into account (name, balance) value (" \
-              "'chengliu', 2000);"
-cursor.execute(add_account)
+#生成10个长度为16的激活码
+act_codes = activation_code(10, 16)
+
+for act_code in act_codes:
+    #构造插入语句
+    insert = "insert into codes values ('%s');"
+    #添加激活码到数据库中
+    cursor.execute(insert % act_code)
+    #提交事务
+    cnx.commit()
+
+#关闭光标
 cursor.close()
+#关闭连接
 cnx.close()
-
-
