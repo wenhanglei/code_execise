1441644d20e990ba2a7982eac4f73e8d7e73343d
wenhanglei <3188744608@qq.com>
2017-11-22 14:49:17 +0800

    二维数组查找

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 ## my coding notebook
+
+***
+
+[Array](src/array)
+
+- [二维数组中的查找](src/array/SolutionFor_01.java)
+
+
---
+++ src/array/SolutionFor_01.java
@@ -0,0 +1,18 @@
+package array;
+/**
+ * 在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+ * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ * @author Administrator
+ *
+ */
+public class SolutionFor_01 {
+	/*
+	 * 思路：
+	 * 矩阵向上和向右都是递增的
+	 * 
+	 */
+	public static boolean find(int[][] array, int i) {
+		
+		return false;
+	}
+}
