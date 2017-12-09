c4ef66b1cc5bb918d72737ac6c8df21e28f8e136
wenhanglei <3188744608@qq.com>
2017-12-09 21:26:05 +0800

    二维数组查找

--- src/array/SolutionFor_01.java
+++ src/array/SolutionFor_01.java
@@ -13,6 +13,30 @@ public class SolutionFor_01 {
 	 */
 	public static boolean find(int[][] array, int i) {
 		
+		int r = array.length;                      //数组多少行
+		int c = array[0].length;                   //数组多少列
+		
+		//当前用于比较的数据
+		int current;
+		for(int j = r-1, k = 0; j >= 0 && k < c;){
+			current = array[j][k];
+			if(current == i) return true;
+			else if(i < current)
+				j = j-1;
+			else if(i > current)
+				k = k+1;
+		}
 		return false;
+	}
+	
+	public static void main(String[] args) {
+		int[][] array = {
+				{0, 2, 3, 4, 5, 6},
+				{1, 3, 4, 5, 6, 7},
+				{2, 4, 5, 6, 7, 8},
+				{3, 5, 6, 7, 8, 9},
+				{4, 6, 7, 8, 9, 10}
+		};
+		System.out.println(find(array, 11));
 	}
 }
