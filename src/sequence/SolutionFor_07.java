edac82e649b48002981768dc2f2027d4e3e56ff2
wenhanglei <3188744608@qq.com>
2017-12-18 16:01:08 +0800

    三种方法实现斐波那契数列

--- src/sequence/SolutionFor_07.java
+++ src/sequence/SolutionFor_07.java
@@ -4,13 +4,15 @@ package sequence;
  *
  */
 public class SolutionFor_07 {
+	
+	//保存下标n为第n项斐波那契数的数组
+	private int[] record;
+	
 	/*
 	 * 思路：
 	 * 递归和动态规划，要注意递归会造成重复计算问题
 	 * 如何避免重复计算
-	 */
-//	private static int[] record;
-	
+	 */	
 	public int Fibonacci(int n) {
 //		if(n <= 1) return n;
 //		if(record == null){
@@ -22,13 +24,41 @@ public class SolutionFor_07 {
 //		if(n < record.length)
 //			return record[n];
 //		else
+		if(n <=1) return n;
 		
-		int[] record = new int[n+1];
-		record[0] = 0;
-		record[1] = 1;
-		for(int i = 2; i < n+1; i++)
+		//如果斐波那契数组为null，初始化该斐波那契数组
+		if(record == null) {
+			record = new int[n+1];
+			record[0] = 0;
+			record[1] = 1;
+		}
+		//-------------------------------------------------
+		//扩容不科学！！！！！！！！！！！！！！！！
+		//如果斐波那契数组的项数小于当前项要求的斐波那契项数，重新创建新的斐波那契数组
+		if(record.length <= n) {
+			int[] temp = new int[n+1];
+			for(int i = 0; i < record.length; i++)
+				temp[i] = record[i];
+			record = temp;
+		}
+		//---------------------------------------------------
+		if(record[n] != 0) return record[n];
+		//计算各项斐波那契数
+		for(int i = 2; i < n+1; i++) {
 			record[i] = record[i-1] + record[i-2];
+		}
 		return record[n];
+		
+		
+		//------------------------------------------------
+		//这是动态规划的解决方案
+//		int[] record = new int[n+1];
+//		record[0] = 0;
+//		record[1] = 1;
+//		for(int i = 2; i < n+1; i++)
+//			record[i] = record[i-1] + record[i-2];
+//		return record[n];
+		//--------------------------------------------------
     }
 //	public int Fibonacci(int n) {
 //		if(n <= 1) return n;
