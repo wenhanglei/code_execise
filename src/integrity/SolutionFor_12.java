2bbd0d06ab4a4d3082a0430903fbd90ceb32a58e
wenhanglei <3188744608@qq.com>
2017-12-22 15:18:05 +0800

    快速幂

--- src/integrity/SolutionFor_12.java
+++ src/integrity/SolutionFor_12.java
@@ -5,9 +5,31 @@ package integrity;
 public class SolutionFor_12 {
 	/*
 	 * 思路：
-	 * ????????
+	 * 
 	 */
 	public double Power(double base, int exponent) {
-        return Math.pow(base, exponent);
+		double res = 1, curr = base;
+		int exp = exponent;
+		if(exp < 0) 
+			exp = -exponent;
+		else if(exp == 0)
+			exp = 0;
+		if(exp == 0) return 1;
+		//快速幂
+		while(exp != 0){
+			if((exp&1) == 1) 
+				res *= curr;
+			curr *= curr;
+			exp >>= 1;
+		}
+		return (exponent > 0)?res:1.0/res;
+	}
+	
+	public static void main(String[] args) {
+		// 11 1011
+		SolutionFor_12 solution = new SolutionFor_12();
+		System.out.println(solution.Power(2.0, 3));
+		System.out.println(solution.Power(2.0, -3));
+		System.out.println(solution.Power(2.0, 0));
 	}
 }
