dd6af950971fcd6a741bcc6a59b9cb983709f2c5
wenhanglei <3188744608@qq.com>
2017-12-24 14:22:47 +0800

    使用两个队列完成调整数组中的数字顺序

--- src/array/SolutionFor_13.java
+++ src/array/SolutionFor_13.java
@@ -1,6 +1,9 @@
 package array;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingDeque;
 
 /**
  * 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，
@@ -13,23 +16,16 @@ public class SolutionFor_13 {
 	 * 
 	 */
 	public void reOrderArray(int [] array) {
-		int a=0, b=0;                                  //a存奇数的下标，b存偶数的下标
-		boolean flag = false;                           //是否发生交换
-		while(!flag) {
-			flag = true;
-	        for(int i = 0; i < array.length; i++) {
-	        	if(array[i]%2 == 0) b = i;             //保存偶数下标
-	        	else a = i;                            //保存偶数下标
-	        	if(a > b){
-	        		exch(array, a, b);
-	        		//交换a，b的值
-	        		int temp = a;
-	        		a = b;
-	        		b = temp;
-	        		flag = false;
-	        	}
-	        }
+		//准备两个队列
+		Queue<Integer> q1 = new LinkedBlockingDeque<Integer>();
+		Queue<Integer> q2 = new LinkedBlockingDeque<Integer>();
+		for(int i = 0; i < array.length; i++) {
+			if(array[i]%2 != 0) q1.add(array[i]);
+			else q2.add(array[i]);
 		}
+		int i = 0; 
+		for(Integer j : q1) array[i++]=j;
+		for(Integer k : q2) array[i++]=k;
     }
 	
 	/**
