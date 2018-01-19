a9bc39463bdf37475e588c686575b1e368a42589
wenhanglei <3188744608@qq.com>
2018-01-19 19:26:35 +0800

    递归求树的深度

--- src/tree/SolutionFor_38.java
+++ src/tree/SolutionFor_38.java
@@ -4,19 +4,20 @@ package tree;
  * 从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。
  */
 public class SolutionFor_38 {
-	int depth;
+	int maxDepth;
 	/*
 	 * 思路:
 	 * 先序遍历
 	 */
 	public int TreeDepth(TreeNode root) {
-        if(root == null) return depth;
-        preorder(root);
-        return depth;
+        if(root == null) return maxDepth;
+        preorder(root, 0);
+        return maxDepth;
     }
-    private void preorder(TreeNode n) {
+    private void preorder(TreeNode n, int depth) {
         if(n == null) return;
-        preorder(n.left);
-        preorder(n.right);
+        if(depth > maxDepth) maxDepth = depth;
+        preorder(n.left, depth+1);
+        preorder(n.right, depth+1);
     }
 }
