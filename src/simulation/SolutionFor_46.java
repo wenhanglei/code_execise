fae73b8f8599876472551cdddc6091969f374cb5
wenhanglei <3188744608@qq.com>
2018-01-27 16:47:43 +0800

    循环实现

--- src/simulation/SolutionFor_46.java
+++ src/simulation/SolutionFor_46.java
@@ -1,4 +1,7 @@
 package simulation;
+
+import java.util.ArrayList;
+
 /**
  * 每年六一儿童节,牛客都会准备一些小礼物去看望孤儿院的小朋友,今年亦是如此。
  * HF作为牛客的资深元老,自然也准备了一些小游戏。
@@ -10,7 +13,48 @@ package simulation;
  * 请你试着想下,哪个小朋友会得到这份礼品呢？(注：小朋友的编号是从0到n-1)
  */
 public class SolutionFor_46 {
+	
+	private int index;
+	/*
+	 * 思路：
+	 */
 	public int LastRemaining_Solution(int n, int m) {
-        return 0;
+        //边界条件判定
+		if(n == 0) return -1;
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for(int i = 0; i < n; i++){
+			list.add(i);
+		}
+		int index = (m-1)%n;
+		for(int i = 0; i < n-1; i++){
+			list.remove(index);
+			index += m-1;
+			index %= list.size();
+		}
+		return list.get(0);
     }
+	
+	
+	
+	/**
+	 * n表示剩下的小朋友的数量
+	 * 上一个处理的小标
+	 * 返回下一个要处理的下标
+	 */
+	private int next(boolean[] arr, int curr){
+		curr++;
+		curr %= arr.length;
+		while(arr[curr])
+			curr++;
+		return curr;
+	}
+	
+	
+	/**
+	 * 测试函数
+	 */
+	public static void main(String[] args) {
+		SolutionFor_46 solution = new SolutionFor_46();
+		System.out.println(solution.LastRemaining_Solution(5, 3));
+	}
 }
