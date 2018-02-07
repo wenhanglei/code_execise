220c8680f8c266e19a5f84f61f7146dd9e35f5d1
wenhanglei <3188744608@qq.com>
2018-02-07 14:52:50 +0800

    判断字符串是否为数值

--- src/string/SolutionFor_53.java
+++ src/string/SolutionFor_53.java
@@ -1,4 +1,7 @@
 package string;
+
+import java.util.Stack;
+
 /**
  * 请实现一个函数用来判断字符串是否表示数值（包括整数和小数）。
  * 例如，字符串"+100","5e2","-123","3.1416"和"-1E-16"都表示数值。 
@@ -7,26 +10,54 @@ package string;
  */
 public class SolutionFor_53 {
 	public boolean isNumeric(char[] str) {
-		//边界判断
-		if(str == null || str.length == 0) return false;
-		
-		char curr = str[0];
-		//如果第一个字符符合规则
-		if(curr == 43 || curr == 45 || (curr > 47 && curr < 58)){
-			
-		}
-		
-        return false;
+		//符号栈
+		Stack<Character> stack = new Stack<Character>();
+        return isNumeric(str, 0, stack);
     }
 	
-	private boolean isNumeric(char[] str, int i){
-		return false;
+	public boolean isNumeric(char[] str, int i, Stack<Character> stack){
+		if(i != str.length){
+			char c = str[i];
+			if(c >= '0' && c <= '9')
+				return isNumeric(str, i+1, stack);
+			else if(c == '+' || c == '-'){
+				if(!stack.isEmpty()){
+					char oper = stack.peek();
+					if(oper == '+' || oper == '-')
+						return false;
+				}else if(i != 0) return false;
+				stack.push(c);
+				return isNumeric(str, i+1, stack);
+			}else if(c == '.'){
+				if(!stack.isEmpty()){
+					if(stack.contains('e') || stack.contains('E') || stack.peek() == '.')
+						return false;
+				}
+				stack.push(c);
+				return isNumeric(str, i+1, stack);
+			}else if(c == 'e' || c == 'E'){
+				if(!stack.isEmpty()){
+					if(stack.peek() == 'e' || stack.peek() == 'E')
+						return false;
+				}
+				stack.push(c);
+				return isNumeric(str, i+1, stack);
+			}
+			return false;
+		}else {
+			if(str[i-1] == 'e' || str[i-1] == 'E') return false;
+			return true;
+		}
 	}
 	
 	public static void main(String[] args) {
-		System.out.println((int)'+');
-		System.out.println((int)'-');
-		System.out.println((int)'0');
-		System.out.println((int)'9');
+		/**
+		 * 字符串"+100","5e2","-123","3.1416"和"-1E-16"都表示数值。 
+ * 但是"12e","1a3.14","1.2.3","+-5"和"12e+4.3"都不是。
+		 */
+		String str = "1+123";
+		SolutionFor_53 sol = new SolutionFor_53();
+		System.out.println(sol.isNumeric(str.toCharArray()));
+		
 	}
 }
