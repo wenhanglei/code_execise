44166bfb1a13a843f148cacfddbe1747adb408ed
wenhanglei <3188744608@qq.com>
2018-02-11 14:27:32 +0800

    删除链表中的重复结点

--- src/linklist/SolutionFor_56.java
+++ src/linklist/SolutionFor_56.java
@@ -8,21 +8,37 @@ public class SolutionFor_56 {
 	public ListNode deleteDuplication(ListNode pHead){
 		//检查边界条件
 		if(pHead == null || pHead.next == null) return pHead;
-		return deleteDup(pHead);
+		ListNode curr = pHead;
+		while(curr.next != null && curr.next.val == curr.val){
+			while(curr.next != null && curr.next.val == curr.val){
+				curr = curr.next;
+			}
+			if(curr.next == null) return null;
+			else {
+				curr = curr.next;
+				pHead = curr;
+			}
+		}
+		if(curr.next == null) return pHead;
+		while(curr.next != null){
+			ListNode pf = curr.next;
+			ListNode ps = curr.next.next;
+			if(ps != null){
+				if(pf.val != ps.val){
+					curr = pf;
+				}else{
+					while(ps != null && pf.val == ps.val){
+						ps = ps.next;
+					}
+					curr.next = ps;
+				}
+			}else{
+				curr = pf;
+			}
+		}
+		return pHead;
     }
 	
-	private ListNode deleteDup(ListNode curr){
-		if(curr == null) return null;
-		ListNode next = deleteDup(curr.next);
-		if(next == null) return curr;
-		if(curr.val != next.val){
-			curr.next = next;
-			return curr;
-		}else {
-			return next.next;
-		}
-		
-	}
 	
 	/**
 	 * 测试函数
@@ -31,9 +47,11 @@ public class SolutionFor_56 {
 	public static void main(String[] args) {
 		int[] arr = {1, 1, 1, 1, 1};
 		int[] arr1 = {1, 1, 2, 2, 3, 3, 4, 4};
-		ListNode head = LinkListUtils.getLinkList(arr);
+		int[] arr2 = {1, 1, 1, 3};
+		int[] arr3 = {1, 2, 3, 3, 4, 4, 5};
+		ListNode head = LinkListUtils.getLinkList(arr2);
 		
 		SolutionFor_56 sol = new SolutionFor_56();
-		sol.deleteDuplication(head);
+		System.out.println(sol.deleteDuplication(head));
 	}
 }
