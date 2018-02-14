e9b5523bd6c0bdc5309007678e1f8e505ed598ee
wenhanglei <3188744608@qq.com>
2018-02-14 23:10:50 +0800

    返回下一个结点的简单朴素的解法

--- src/tree/SolutionFor_57.java
+++ src/tree/SolutionFor_57.java
@@ -1,17 +1,42 @@
 package tree;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
 /**
  * 给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回。
  * 注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针。
  * @author wenhanglei
  */
 public class SolutionFor_57 {
+	
+	private LinkedList<TreeLinkNode> list = new LinkedList<TreeLinkNode>();
+	
 	public TreeLinkNode GetNext(TreeLinkNode pNode){
 		if(pNode == null) return null;
+		//查找根节点
+		TreeLinkNode curr = pNode;
+		while(curr.next != null){
+			curr = curr.next;
+		}
+		//中序遍历根节点
+		midOrder(curr);
+		Iterator<TreeLinkNode> iterator = list.iterator();
+		while(iterator.hasNext()){
+			if(pNode == iterator.next()){
+				if(iterator.hasNext())
+					return iterator.next();
+				else return null;
+			}
+		}
         return null;
     }
 	
-	private TreeLinkNode midOrder(TreeLinkNode node){
-		return null;
+	private void midOrder(TreeLinkNode curr){
+		if(curr == null) return;
+		midOrder(curr.left);
+		list.add(curr);
+		midOrder(curr.right);
 	}
 	
 	/**
@@ -21,5 +46,7 @@ public class SolutionFor_57 {
 		int[] arr = {2, 3, '#', 6, 8, '#'};
 		TreeLinkNode node = TreeUtils.getTree(arr);
 		TreeLinkNode n = TreeUtils.getNode(node, 6);
+		SolutionFor_57 sol = new SolutionFor_57();
+		System.out.println(sol.GetNext(n));
 	}
 }
