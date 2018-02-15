1312fa367f6739422c71ce82e3f20dcaf0bdb955
wenhanglei <3188744608@qq.com>
2018-02-15 15:16:24 +0800

    查找中序遍历的下一个结点

--- src/tree/SolutionFor_57.java
+++ src/tree/SolutionFor_57.java
@@ -1,8 +1,5 @@
 package tree;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-
 /**
  * 给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回。
  * 注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针。
@@ -10,34 +7,30 @@ import java.util.LinkedList;
  */
 public class SolutionFor_57 {
 	
-	private LinkedList<TreeLinkNode> list = new LinkedList<TreeLinkNode>();
-	
+	/*
+	 * 思路：
+	 * 右子树的的最左边的结点
+	 * 当没有右子树的时候是第一个右父节点
+	 */
 	public TreeLinkNode GetNext(TreeLinkNode pNode){
 		if(pNode == null) return null;
-		//查找根节点
-		TreeLinkNode curr = pNode;
-		while(curr.next != null){
+		TreeLinkNode curr = null;
+		//检查右子树是否为null
+		if(pNode.right != null){
+			curr = pNode.right;
+			while(curr.left != null){
+				curr = curr.left;
+			}
+			if(curr != null) return curr;
+		}
+		//当没有右子树的时候返回第一个右父节点
+		curr = pNode;
+		while(curr.next != null && curr.next.right == curr){
 			curr = curr.next;
 		}
-		//中序遍历根节点
-		midOrder(curr);
-		Iterator<TreeLinkNode> iterator = list.iterator();
-		while(iterator.hasNext()){
-			if(pNode == iterator.next()){
-				if(iterator.hasNext())
-					return iterator.next();
-				else return null;
-			}
-		}
-        return null;
+		if(curr.next == null) return null;
+		return curr.next;
     }
-	
-	private void midOrder(TreeLinkNode curr){
-		if(curr == null) return;
-		midOrder(curr.left);
-		list.add(curr);
-		midOrder(curr.right);
-	}
 	
 	/**
 	 * 测试函数
@@ -45,7 +38,7 @@ public class SolutionFor_57 {
 	public static void main(String[] args) {
 		int[] arr = {2, 3, '#', 6, 8, '#'};
 		TreeLinkNode node = TreeUtils.getTree(arr);
-		TreeLinkNode n = TreeUtils.getNode(node, 6);
+		TreeLinkNode n = TreeUtils.getNode(node, 2);
 		SolutionFor_57 sol = new SolutionFor_57();
 		System.out.println(sol.GetNext(n));
 	}
